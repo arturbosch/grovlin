4ac2c2e1c0c622cc7c8dfd31584f466bbb0de0ea
abosch <arturbosch@gmx.de>
2017-02-06 21:15:22 +0100

    Add functions to parse positions from antlr tokens - #1

--- grovlin-parser/src/main/kotlin/io/gitlab/arturbosch/grovlin/parser/ast/Positions.kt
+++ grovlin-parser/src/main/kotlin/io/gitlab/arturbosch/grovlin/parser/ast/Positions.kt
@@ -1,8 +1,7 @@
 package io.gitlab.arturbosch.grovlin.parser.ast
 
-/**
- * @author Artur Bosch
- */
+import org.antlr.v4.runtime.ParserRuleContext
+import org.antlr.v4.runtime.Token
 
 data class Point(val line: Int, val column: Int) {
 
@@ -35,4 +34,22 @@ data class Position(val start: Point, val end: Point) {
 		val lines = code.split("\n")
 		return end.offset(lines) - start.offset(lines)
 	}
+}
+
+fun pos(startLine: Int, startColumn: Int, endLine: Int, endColumn: Int) = Position(Point(startLine, startColumn), Point(endLine, endColumn))
+
+// Node extensions
+
+fun Node.isBefore(other: Node): Boolean = position != null && other.position != null && position!!.start.isBefore(other.position!!.start)
+
+fun Node.isAfter(other: Node): Boolean = position != null && other.position != null && position!!.start.isAfter(other.position!!.start)
+
+// Grovlin Parser
+
+fun Token.startPoint() = Point(line, charPositionInLine)
+
+fun Token.endPoint() = Point(line, charPositionInLine + text.length)
+
+fun ParserRuleContext.toPosition(): Position? {
+	return Position(start.startPoint(), stop.endPoint())
 }
