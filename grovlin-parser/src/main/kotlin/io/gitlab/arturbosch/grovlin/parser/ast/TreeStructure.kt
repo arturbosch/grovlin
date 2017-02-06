2fd69ecc16cffdbfdff5a9aa24c47da6b25179b8
abosch <arturbosch@gmx.de>
2017-02-06 21:12:50 +0100

    Introduce positions to all nodes - #1

--- grovlin-parser/src/main/kotlin/io/gitlab/arturbosch/grovlin/parser/ast/TreeStructure.kt
+++ grovlin-parser/src/main/kotlin/io/gitlab/arturbosch/grovlin/parser/ast/TreeStructure.kt
@@ -3,19 +3,19 @@ package io.gitlab.arturbosch.grovlin.parser.ast
 /**
  * @author Artur Bosch
  */
+
+//
+// Interfaces
+//
+
 interface Node {
+	val position: Position?
 	fun print(indent: Int = 0): String
 }
 
 interface NodeWithName : Node {
 	val name: String
 	fun printTypeAndName(indent: Int = 0): String = "${times(indent)}${javaClass.simpleName}: $name\n"
-}
-
-fun times(times: Int): String {
-	val builder = StringBuilder()
-	for (i in 1..times) builder.append("\t")
-	return builder.toString()
 }
 
 interface NodeWithStatements : Node {
@@ -26,16 +26,23 @@ interface NodeWithStatements : Node {
 	}
 }
 
-interface Type {
-	fun print(indent: Int = 0): String = javaClass.simpleName
-}
-
 interface Expression : Node
+
 interface Statement : Node
+
+fun times(times: Int): String {
+	val builder = StringBuilder()
+	for (i in 1..times) builder.append("\t")
+	return builder.toString()
+}
 
 //
 // Types
 //
+
+interface Type {
+	fun print(indent: Int = 0): String = javaClass.simpleName
+}
 
 object IntType : Type
 
@@ -45,13 +52,13 @@ object DecimalType : Type
 // File & Program
 //
 
-data class GrovlinFile(override val name: String, override val statements: MutableList<Statement>) : NodeWithStatements, NodeWithName {
+data class GrovlinFile(override val name: String, override val statements: MutableList<Statement>, override val position: Position? = null) : NodeWithStatements, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent) + printStatements(indent + 1)
 	}
 }
 
-data class Program(override val statements: MutableList<Statement>) : Statement, NodeWithStatements {
+data class Program(override val statements: MutableList<Statement>, override val position: Position? = null) : Statement, NodeWithStatements {
 	override fun print(indent: Int): String {
 		return "Program:\n" + printStatements(indent + 1)
 	}
@@ -61,43 +68,43 @@ data class Program(override val statements: MutableList<Statement>) : Statement,
 // Statements
 //
 
-data class TypeDeclaration(override val name: String, override val statements: MutableList<Statement>) : Statement, NodeWithStatements, NodeWithName {
+data class TypeDeclaration(override val name: String, override val statements: MutableList<Statement>, override val position: Position? = null) : Statement, NodeWithStatements, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + printStatements(indent + 2)
 	}
 }
 
-data class MethodDeclaration(override val name: String, override val statements: MutableList<Statement>) : Statement, NodeWithStatements, NodeWithName {
+data class MethodDeclaration(override val name: String, override val statements: MutableList<Statement>, override val position: Position? = null) : Statement, NodeWithStatements, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + printStatements(indent + 2)
 	}
 }
 
-data class LambdaDeclaration(override val name: String, override val statements: MutableList<Statement>) : Statement, NodeWithStatements, NodeWithName {
+data class LambdaDeclaration(override val name: String, override val statements: MutableList<Statement>, override val position: Position? = null) : Statement, NodeWithStatements, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + printStatements(indent + 2)
 	}
 }
 
-data class PropertyDeclaration(override val name: String, val value: Expression) : Statement, NodeWithName {
+data class PropertyDeclaration(override val name: String, val value: Expression, override val position: Position? = null) : Statement, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + value.print(indent + 2)
 	}
 }
 
-data class VarDeclaration(override val name: String, val value: Expression) : Statement, NodeWithName {
+data class VarDeclaration(override val name: String, val value: Expression, override val position: Position? = null) : Statement, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + value.print(indent + 2)
 	}
 }
 
-data class Assignment(override val name: String, val value: Expression) : Statement, NodeWithName {
+data class Assignment(override val name: String, val value: Expression, override val position: Position? = null) : Statement, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1) + value.print(indent + 2)
 	}
 }
 
-data class Print(val value: Expression) : Statement {
+data class Print(val value: Expression, override val position: Position? = null) : Statement {
 	override fun print(indent: Int): String {
 		return "Print:\n" + value.print(indent + 1)
 	}
@@ -121,34 +128,34 @@ interface BinaryExpression : Expression {
 	}
 }
 
-data class SumExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.sum) : BinaryExpression
+data class SumExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.sum, override val position: Position? = null) : BinaryExpression
 
-data class SubtractionExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.sub) : BinaryExpression
+data class SubtractionExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.sub, override val position: Position? = null) : BinaryExpression
 
-data class MultiplicationExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.mul) : BinaryExpression
+data class MultiplicationExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.mul, override val position: Position? = null) : BinaryExpression
 
-data class DivisionExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.div) : BinaryExpression
+data class DivisionExpression(override val left: Expression, override val right: Expression, override val operator: Operator = Operator.div, override val position: Position? = null) : BinaryExpression
 
-data class UnaryMinusExpression(val value: Expression) : Expression {
+data class UnaryMinusExpression(val value: Expression, override val position: Position? = null) : Expression {
 	override fun print(indent: Int): String {
 		return "${javaClass.simpleName}\n${times(indent)}${value.print(indent + 1)}"
 	}
 }
 
-data class TypeConversion(val value: Expression, val targetType: Type) : Expression {
+data class TypeConversion(val value: Expression, val targetType: Type, override val position: Position? = null) : Expression {
 	override fun print(indent: Int): String = "${javaClass.simpleName}\n${times(indent)}${value.print(indent + 1)} as ${targetType.print(indent + 1)}"
 }
 
-data class VarReference(override val name: String) : Expression, NodeWithName {
+data class VarReference(override val name: String, override val position: Position? = null) : Expression, NodeWithName {
 	override fun print(indent: Int): String {
 		return printTypeAndName(indent + 1)
 	}
 }
 
-data class IntLit(val value: String) : Expression {
+data class IntLit(val value: String, override val position: Position? = null) : Expression {
 	override fun print(indent: Int): String = times(indent + 1) + value
 }
 
-data class DecLit(val value: String) : Expression {
+data class DecLit(val value: String, override val position: Position? = null) : Expression {
 	override fun print(indent: Int): String = times(indent + 1) + value
 }
