e3d71e1882940ea0f0e784dfd5804677075c3eec
abosch <arturbosch@gmx.de>
2017-02-22 04:50:11 +0100

    Move number test case to ast package

--- grovlin-ast/src/test/kotlin/io/gitlab/arturbosch/grovlin/ast/NumberTest.kt
+++ grovlin-ast/src/test/kotlin/io/gitlab/arturbosch/grovlin/ast/NumberTest.kt
@@ -1,11 +1,7 @@
+package io.gitlab.arturbosch.grovlin.ast
+
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.present
-import io.gitlab.arturbosch.grovlin.ast.DivisionExpression
-import io.gitlab.arturbosch.grovlin.ast.IntLit
-import io.gitlab.arturbosch.grovlin.ast.MultiplicationExpression
-import io.gitlab.arturbosch.grovlin.ast.SubtractionExpression
-import io.gitlab.arturbosch.grovlin.ast.SumExpression
-import io.gitlab.arturbosch.grovlin.ast.asGrovlinFile
 import io.gitlab.arturbosch.grovlin.ast.operations.collectByType
 import org.junit.Test
 import kotlin.test.assertTrue
@@ -19,7 +15,7 @@ class NumberTest {
 	fun parseComplexExpressions() {
 		val grovlinFile = "val a = (5 * 5) + (1 + (8 - (4 / 2)))".asGrovlinFile()
 		val sum = grovlinFile.collectByType<SumExpression>()[0]
-		
+
 		assertThat(sum.left.collectByType<MultiplicationExpression>()[0], present())
 		assertThat(sum.right.collectByType<SumExpression>()[0], present())
 		assertThat(sum.right.collectByType<SumExpression>()[0], present())
