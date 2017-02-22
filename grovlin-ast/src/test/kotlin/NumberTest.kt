9e5ca33526c3e3102b9258f13a2af5f5a9bbc559
abosch <arturbosch@gmx.de>
2017-02-22 02:26:26 +0100

    Fix test case for complex arithmetic expressions caused by new paren expression

--- grovlin-ast/src/test/kotlin/NumberTest.kt
+++ grovlin-ast/src/test/kotlin/NumberTest.kt
@@ -1,9 +1,12 @@
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.present
 import io.gitlab.arturbosch.grovlin.ast.DivisionExpression
 import io.gitlab.arturbosch.grovlin.ast.IntLit
 import io.gitlab.arturbosch.grovlin.ast.MultiplicationExpression
 import io.gitlab.arturbosch.grovlin.ast.SubtractionExpression
 import io.gitlab.arturbosch.grovlin.ast.SumExpression
 import io.gitlab.arturbosch.grovlin.ast.asGrovlinFile
+import io.gitlab.arturbosch.grovlin.ast.operations.asString
 import io.gitlab.arturbosch.grovlin.ast.operations.collectByType
 import org.junit.Test
 import kotlin.test.assertTrue
@@ -17,11 +20,14 @@ class NumberTest {
 	fun parseComplexExpressions() {
 		val grovlinFile = "val a = (5 * 5) + (1 + (8 - (4 / 2)))".asGrovlinFile()
 		val sum = grovlinFile.collectByType<SumExpression>()[0]
-
-		assertTrue(sum.left is MultiplicationExpression)
-		assertTrue(sum.right is SumExpression)
-		assertTrue((sum.right as SumExpression).left is IntLit)
-		assertTrue((sum.right as SumExpression).right is SubtractionExpression)
-		assertTrue(((sum.right as SumExpression).right as SubtractionExpression).right is DivisionExpression)
+		println(grovlinFile.asString())
+		assertThat(sum.left.collectByType<MultiplicationExpression>()[0], present())
+		assertThat(sum.right.collectByType<SumExpression>()[0], present())
+		assertThat(sum.right.collectByType<SumExpression>()[0], present())
+		assertTrue(sum.right.collectByType<SumExpression>()[0].left is IntLit)
+		assertThat(sum.right.collectByType<SumExpression>()[0].right.collectByType<SubtractionExpression>(), present())
+		assertTrue(sum.right.collectByType<SumExpression>()[0].right.collectByType<SubtractionExpression>()[0].left is IntLit)
+		assertThat(sum.right.collectByType<SumExpression>()[0].right.collectByType<SubtractionExpression>()[0].right
+				.collectByType<DivisionExpression>()[0], present())
 	}
 }
