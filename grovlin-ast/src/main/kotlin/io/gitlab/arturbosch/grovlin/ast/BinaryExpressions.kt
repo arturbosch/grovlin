2e02f03c976da9fa0f13c8feefcc6b6a4f8f1ebb
Artur Bosch <arturbosch@gmx.de>
2017-07-30 17:24:12 +0200

    Reformat binary expressions

--- grovlin-ast/src/main/kotlin/io/gitlab/arturbosch/grovlin/ast/BinaryExpressions.kt
+++ grovlin-ast/src/main/kotlin/io/gitlab/arturbosch/grovlin/ast/BinaryExpressions.kt
@@ -12,40 +12,53 @@ abstract class RelationExpression(left: Expression,
 	: BinaryExpression(left, right)
 
 class SumExpression(left: Expression,
-					right: Expression) : BinaryExpression(left, right)
+					right: Expression)
+	: BinaryExpression(left, right)
 
 class SubtractionExpression(left: Expression,
-							right: Expression) : BinaryExpression(left, right)
+							right: Expression)
+	: BinaryExpression(left, right)
 
 class MultiplicationExpression(left: Expression,
-							   right: Expression) : BinaryExpression(left, right)
+							   right: Expression)
+	: BinaryExpression(left, right)
 
 class DivisionExpression(left: Expression,
-						 right: Expression) : BinaryExpression(left, right)
+						 right: Expression)
+	: BinaryExpression(left, right)
 
 class AndExpression(left: Expression,
-					right: Expression) : BinaryExpression(left, right)
+					right: Expression)
+	: BinaryExpression(left, right)
 
 class OrExpression(left: Expression,
-				   right: Expression) : BinaryExpression(left, right)
+				   right: Expression)
+	: BinaryExpression(left, right)
 
 class XorExpression(left: Expression,
-					right: Expression) : BinaryExpression(left, right)
+					right: Expression)
+	: BinaryExpression(left, right)
 
 class EqualExpression(left: Expression,
-					  right: Expression) : RelationExpression(left, right)
+					  right: Expression)
+	: RelationExpression(left, right)
 
 class UnequalExpression(left: Expression,
-						right: Expression) : RelationExpression(left, right)
+						right: Expression)
+	: RelationExpression(left, right)
 
 class LessEqualExpression(left: Expression,
-						  right: Expression) : RelationExpression(left, right)
+						  right: Expression)
+	: RelationExpression(left, right)
 
 class LessExpression(left: Expression,
-					 right: Expression) : RelationExpression(left, right)
+					 right: Expression)
+	: RelationExpression(left, right)
 
 class GreaterExpression(left: Expression,
-						right: Expression) : RelationExpression(left, right)
+						right: Expression)
+	: RelationExpression(left, right)
 
 class GreaterEqualExpression(left: Expression,
-							 right: Expression) : RelationExpression(left, right)
+							 right: Expression)
+	: RelationExpression(left, right)
