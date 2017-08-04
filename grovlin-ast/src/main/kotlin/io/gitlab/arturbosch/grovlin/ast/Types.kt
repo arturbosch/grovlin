963e0adf9cd5c4f42ea3a52cb9779fed58afe6d3
Artur Bosch <arturbosch@gmx.de>
2017-08-04 10:03:36 +0200

    Make types also nodes

--- grovlin-ast/src/main/kotlin/io/gitlab/arturbosch/grovlin/ast/Types.kt
+++ grovlin-ast/src/main/kotlin/io/gitlab/arturbosch/grovlin/ast/Types.kt
@@ -6,7 +6,7 @@ import io.gitlab.arturbosch.grovlin.ast.builtins.StringType
  * @author Artur Bosch
  */
 
-interface Type : Named {
+abstract class Type : Node(), NodeWithName {
 	companion object {
 		fun of(type: String) = when (type) {
 			"Bool" -> BoolType
@@ -19,13 +19,13 @@ interface Type : Named {
 	}
 }
 
-interface NumberType : Type
+abstract class NumberType : PrimitiveType()
 
-abstract class PrimitiveType : Type {
+abstract class PrimitiveType : Type() {
 	override fun toString(): String = name
 }
 
-open class ObjectOrTypeType(override val name: String) : Type {
+open class ObjectOrTypeType(override val name: String) : Type() {
 	override fun toString(): String = name
 }
 
@@ -33,20 +33,20 @@ object BoolType : PrimitiveType() {
 	override val name: String = "Bool"
 }
 
-object IntType : PrimitiveType(), NumberType {
+object IntType : NumberType() {
 	override val name: String = "Int"
 }
 
-object DecimalType : PrimitiveType(), NumberType {
+object DecimalType : NumberType() {
 	override val name: String = "Decimal"
 }
 
-object UnknownType : Type {
+object UnknownType : Type() {
 	override val name: String = "Unknown"
 	override fun toString(): String = name
 }
 
-object VoidType : Type {
+object VoidType : Type() {
 	override val name: String = "Void"
 	override fun toString(): String = name
 }
